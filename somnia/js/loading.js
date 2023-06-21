2884922f89e40367c20e1a7136e98a8c5314651d
sumin8203 <113405664+dxdsumin@users.noreply.github.com>
2023-06-22 05:07:42 +0900

    mkl

---
+++ somnia/js/loading.js
@@ -0,0 +1,19 @@
+
+const load = document.querySelector('.loading_message');
+const clickload = document.querySelector('.Logo');
+window.addEventListener('DOMContentLoaded', function()
+{
+    console.log('test');
+    load.style.visibility = 'visible';
+});
+
+window.onload = () =>{
+    console.log('투명화');
+    load.style.visibility = 'hidden';
+}
+
+clickload.addEventListener('click', function(){
+    console.log("로딩중");
+    load.style.visibility = 'visible';
+    clickload.style.visibility = 'hidden';
+})
